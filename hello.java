70035f3d52ed2ab4ce2ba4d6e4dabb54c4cecdf8
Sinchana12345CSE <129932812+Sinchana12345CSE@users.noreply.github.com>
2023-04-23 13:48:37 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello{
 		System.out.println("Hello World!");
 		System.out.println("hello india");
 		System.out.println("welcome spt");
+		System.out.println("tumkur batawadi");
 	}
 }
