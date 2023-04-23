81a4bb6ed5f3dbb173e36ef84dcbe87e7d73e911
sinchu <sinchu@gmail.com>
2023-04-23 13:39:17 +0530

    change3

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
 	public static void main(String []args){
 		System.out.println("Hello World!");
 		System.out.println("hello india");
+		System.out.println("welcome spt");
 	}
 }
